254400a55465986c2053ae9c871e2a7da6c8fc80
Satyendra <satyendra@192.168.1.3>
2023-09-18 03:16:19 +0530

    refactor

--- src/main/java/com/typeface/assignment/filestorageservice/infrastructure/external/s3/AwsS3Config.java
+++ src/main/java/com/typeface/assignment/filestorageservice/infrastructure/external/s3/AwsS3Config.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 public class AwsS3Config {
     @Bean
     public AmazonS3 amazonS3() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials("AKIAT563BDZSD7K3BQHU", "JO1mcgMdZQ6kbNNu6RgNUiw3W7MwkZSYEa2zqbCe");
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials("*********", "*********");
         return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
